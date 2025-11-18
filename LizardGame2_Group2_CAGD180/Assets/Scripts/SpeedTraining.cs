e77957de4d5c49574e762da8ad8b452e2f1b4153
TopherOverbeyDrac <topher.overbey@gmail.com>
2025-11-18 11:48:47 -0800

    SpeedTraining Bace

--- LizardGame2_Group2_CAGD180/Assets/Scripts/SpeedTraining.cs
+++ LizardGame2_Group2_CAGD180/Assets/Scripts/SpeedTraining.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class SpeedTraining : MonoBehaviour
 {
+    public int speedAmount;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+    public void SpaceButtonClickCount()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+
+        }
         
+
     }
 }
