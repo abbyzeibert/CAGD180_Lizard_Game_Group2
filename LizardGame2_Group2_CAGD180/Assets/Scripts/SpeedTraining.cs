e0f6e2911ebc83acfc8f99908797b77edae31fc3
TopherOverbeyDrac <topher.overbey@gmail.com>
2025-11-20 10:26:48 -0800

    Update SpeedTraining.cs

--- LizardGame2_Group2_CAGD180/Assets/Scripts/SpeedTraining.cs
+++ LizardGame2_Group2_CAGD180/Assets/Scripts/SpeedTraining.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class SpeedTraining : MonoBehaviour
 {
-    public int speedAmount;
+    public int clickAmount = 0;
+    public int speedToAdd = 0;
+    public bool isTrainingMoveing = false;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,15 +17,37 @@ public class SpeedTraining : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        SpaceButtonClickCount();
+        if (clickAmount > 100)
+        {
+            speedToAdd = 3;
+        }
+        else if (clickAmount > 50)
+        {
+            speedToAdd = 2;
+        }
+        else
+        {
+            speedToAdd = 1;
+        }
     }
     public void SpaceButtonClickCount()
     {
-        if (Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetKeyUp(KeyCode.Space) && isTrainingMoveing)
         {
-
+            clickAmount++;
         }
-        
+    }
+    public IEnumerator WaitToStart()
+    {
 
+        yield return new WaitForSeconds(3);
+        StartCoroutine(StartTraining());
+    }
+    public IEnumerator StartTraining()
+    {
+        isTrainingMoveing = true;
+        yield return new WaitForSeconds(30);
+        isTrainingMoveing = false;
     }
 }
