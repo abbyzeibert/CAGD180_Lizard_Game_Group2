108e193cda792582c6eee633da4435aa5323a60a
abbyzeibert <abbyzeibert@gmail.com>
2025-12-02 11:00:12 -0800

    leap training basics done

--- LizardGame2_Group2_CAGD180/Assets/Scripts/LeapTraining.cs
+++ LizardGame2_Group2_CAGD180/Assets/Scripts/LeapTraining.cs
@@ -5,32 +5,34 @@ using UnityEngine;
 public class LeapTraining : MonoBehaviour
 {
     public bool gameRunning = false;
+    public bool justJumped = false;
 
     public int leapToAdd = 0;
     public int numJumps = 0;
     public float markerSpeed = 5;
 
     private float markDistance;
-    public float maxDistance = 10;
+    public float maxDistance = 15;
     public int direction = 1;
-    private Vector3 markStart;
+    public GameObject markStart;
 
     public Rigidbody lizard;
     public GameObject marker;
+    public GameManager manager;
 
     // Start is called before the first frame update
     void Start()
     {
-        markStart = marker.transform.position;
+        manager = GameObject.Find("Game Manager").GetComponent<GameManager>();
         StartCoroutine(WaitToStart());
     }
 
     // Update is called once per frame
     void Update()
     {
-        while (gameRunning)
+        if (gameRunning && !justJumped)
         {
-            markDistance = Vector3.Distance(markStart, marker.transform.position);
+            markDistance = Vector3.Distance(markStart.transform.position, marker.transform.position);
 
             if(markDistance >= maxDistance)
             {
@@ -45,26 +47,27 @@ public class LeapTraining : MonoBehaviour
         }
 
 
-        if(gameRunning && Input.GetKeyDown(KeyCode.Space))
+        if(gameRunning && Input.GetKeyDown(KeyCode.Space) && !justJumped)
         {
-            if(markDistance > 9)
+            if(markDistance > 12)
             {
                 numJumps += 3;
-                lizard.AddForce(Vector3.right * 5, ForceMode.Impulse);
-                lizard.AddForce(Vector3.up * 2.5f, ForceMode.Impulse);
+                lizard.AddForce(Vector3.right * 8, ForceMode.Impulse);
+                lizard.AddForce(Vector3.up * 4, ForceMode.Impulse);
             }
-            else if(markDistance > 6)
+            else if(markDistance > 8)
             {
                 numJumps += 2;
-                lizard.AddForce(Vector3.right * 3.5f, ForceMode.Impulse);
-                lizard.AddForce(Vector3.up * 1.75f, ForceMode.Impulse);
+                lizard.AddForce(Vector3.right * 6, ForceMode.Impulse);
+                lizard.AddForce(Vector3.up * 3, ForceMode.Impulse);
             }
             else
             {
                 numJumps += 1;
-                lizard.AddForce(Vector3.right * 2, ForceMode.Impulse);
-                lizard.AddForce(Vector3.up * 1, ForceMode.Impulse);
+                lizard.AddForce(Vector3.right * 4, ForceMode.Impulse);
+                lizard.AddForce(Vector3.up * 2, ForceMode.Impulse);
             }
+            StartCoroutine(JumpTimer());
         }
     }
 
@@ -79,5 +82,26 @@ public class LeapTraining : MonoBehaviour
         gameRunning = true;
         yield return new WaitForSeconds(30);
         gameRunning = false;
+
+        if(numJumps >= 30)
+        {
+            manager.playerLeap += 2;
+        }
+        else if( numJumps >= 15)
+        {
+            manager.playerLeap += 1;
+        }
+
+        yield return new WaitForSeconds(5);
+
+
+    }
+
+    public IEnumerator JumpTimer()
+    {
+        justJumped = true;
+        yield return new WaitForSeconds(1);
+        justJumped = false;
+        marker.transform.position = markStart.transform.position;
     }
 }
